9d5d049a7ef47b6540cb618b37a364dc42658c05
Calvin Cheng <linchuan.cheng@gmail.com>
2016-08-29 22:09:01 +0800

    Configurable verbosity

--- iristester.go
+++ iristester.go
@@ -8,17 +8,30 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-func IrisTester(t *testing.T, handler fasthttp.RequestHandler, baseUrl string) *httpexpect.Expect {
+// IrisTester is a wrapper to help us run end-to-end tests easily
+// testVerbosity 2, 1, others (0)
+func IrisTester(t *testing.T, handler fasthttp.RequestHandler, baseURL string, testVerbosity int) *httpexpect.Expect {
+
+	var printers []httpexpect.Printer
+	if testVerbosity == 2 {
+		printers = []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		}
+	} else if testVerbosity == 1 {
+		printers = []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, false),
+		}
+	} else {
+		printers = nil
+	}
 
 	return httpexpect.WithConfig(httpexpect.Config{
-		BaseURL: baseUrl,
+		BaseURL: baseURL,
 		Client: &http.Client{
 			Transport: httpexpect.NewFastBinder(handler),
 			Jar:       httpexpect.NewJar(),
 		},
 		Reporter: httpexpect.NewAssertReporter(t),
-		Printers: []httpexpect.Printer{
-			httpexpect.NewDebugPrinter(t, true),
-		},
+		Printers: printers,
 	})
 }
